a45b7245c36b605dfb2c7abeb26b51bc7cc18acc
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2015-09-21 14:52:00 +0000

    Added alpha to binary by sorting decoder.

---
+++ clashes/10mn/alpha-binary-sorting.js
@@ -0,0 +1,19 @@
+var str = readline().split('');
+var a, alpha = [];
+
+str.forEach(c => {
+  if (a = alpha.find(b => (b.letter === c))) {
+    a.count++;
+  } else {
+    alpha.push({ letter: c, count: 1 });
+  }
+});
+
+alpha
+.sort((a, b) => (a.count < b.count) ? 1 : -1)
+.map((obj, i) => {
+  obj.binary = '0'.repeat(i) + '1';
+  return obj;
+});
+
+print(str.map(c => alpha.find(a => a.letter === c).binary).join(''));
